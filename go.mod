a7de43204fb7035282279faf20e35cc03a0831e2
Bradley Wojcik <bradleycwojcik@gmail.com>
2024-06-06 15:05:13 -0400

    implement write table to screen with basic stats

--- go.mod
+++ go.mod
@@ -2,8 +2,34 @@ module boldandbrad/pts
 
 go 1.22.3
 
+require github.com/PuerkitoBio/goquery v1.9.2
+
 require (
-	github.com/PuerkitoBio/goquery v1.9.2
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/charmbracelet/bubbles v0.11.0 // indirect
+	github.com/charmbracelet/bubbletea v0.26.4 // indirect
+	github.com/charmbracelet/lipgloss v0.5.0 // indirect
+	github.com/charmbracelet/x/ansi v0.1.2 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/evertras/bubble-table v0.16.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/evertras/bubble-table/table"
 )
 
 var teams map[string]int = map[string]int{
@@ -41,6 +46,57 @@ var teams map[string]int = map[string]int{
 	"TEX": 13,
 	"TOR": 14,
 	"WSN": 24,
+}
+
+const (
+	columnKeyName             = "name"
+	columnKeyPlateAppearances = "pa"
+	columnKeyPosPoints        = "pospts"
+	columnKeyPosPointsPerPA   = "posptsperpa"
+	columnKeyNegPoints        = "negpts"
+	columnKeyNegPointsPerPA   = "negptsperpa"
+	columnKeyPoints           = "pts"
+	columnKeyPointsPerPA      = "ptsperpa"
+)
+
+var (
+	styleSubtle = lipgloss.NewStyle().Foreground(lipgloss.Color("#888"))
+
+	styleBase = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#eee")).
+			BorderForeground(lipgloss.Color("#89b4fa")).
+			Align(lipgloss.Right)
+)
+
+type Stick struct {
+	Name                  string
+	GamesPlayed           int
+	PlateAppearances      int
+	Singles               int
+	Doubles               int
+	Triples               int
+	HomeRuns              int
+	RunsScored            int
+	RunsBattedIn          int
+	StolenBases           int
+	Walks                 int
+	HitByPitches          int
+	SacBunts              int
+	StrikeOuts            int
+	GroundIntoDoublePlays int
+	Errors                int
+}
+
+func (s Stick) PositivePoints() int {
+	return s.Singles + (2 * s.Doubles) + (3 * s.Triples) + (4 * s.HomeRuns) + s.RunsScored + s.RunsBattedIn + s.StolenBases + s.Walks + s.HitByPitches + s.SacBunts
+}
+
+func (s Stick) NegativePoints() int {
+	return s.StrikeOuts + (2 * s.GroundIntoDoublePlays) + s.Errors
+}
+
+type Model struct {
+	statTable table.Model
 }
 
 // fetchHTML fetches the HTML content from a given URL.
@@ -84,6 +140,39 @@ func extractTableData(doc *goquery.Document) ([]string, [][]string, error) {
 	return headers, rows, nil
 }
 
+func MustBeInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func parseSticks(rows [][]string) []Stick {
+	var sticks []Stick
+	for _, row := range rows {
+		stick := Stick{
+			Name:                  row[1],
+			PlateAppearances:      MustBeInt(row[4]),
+			Singles:               MustBeInt(row[6]),
+			Doubles:               MustBeInt(row[7]),
+			Triples:               MustBeInt(row[8]),
+			HomeRuns:              MustBeInt(row[9]),
+			RunsScored:            MustBeInt(row[10]),
+			RunsBattedIn:          MustBeInt(row[11]),
+			StolenBases:           MustBeInt(row[19]),
+			Walks:                 MustBeInt(row[12]),
+			HitByPitches:          MustBeInt(row[15]),
+			SacBunts:              MustBeInt(row[17]),
+			StrikeOuts:            MustBeInt(row[14]),
+			GroundIntoDoublePlays: MustBeInt(row[18]),
+			Errors:                0,
+		}
+		sticks = append(sticks, stick)
+	}
+	return sticks
+}
+
 // writeCSV writes the table data to a CSV file.
 func writeCSV(data [][]string, filename string) error {
 	file, err := os.Create(filename)
@@ -103,7 +192,108 @@ func writeCSV(data [][]string, filename string) error {
 	return nil
 }
 
+func makeRow(stick Stick) table.Row {
+	return table.NewRow(table.RowData{
+		columnKeyName: stick.Name,
+		columnKeyPlateAppearances: func() string {
+			if stick.PlateAppearances > 0 {
+				return fmt.Sprintf("%d", stick.PlateAppearances)
+			}
+			return "0"
+		}(),
+		columnKeyPosPoints: func() string {
+			if stick.PositivePoints() > 0 {
+				return fmt.Sprintf("%d", stick.PositivePoints())
+			}
+			return "0"
+		}(),
+		columnKeyPosPointsPerPA: func() string {
+			if stick.PlateAppearances > 0 {
+				return fmt.Sprintf("%.3f", float64(stick.PositivePoints())/float64(stick.PlateAppearances))
+			}
+			return "0"
+		}(),
+		columnKeyNegPoints: func() string {
+			if stick.NegativePoints() > 0 {
+				return fmt.Sprintf("-%d", stick.NegativePoints())
+			}
+			return "0"
+		}(),
+		columnKeyNegPointsPerPA: func() string {
+			if stick.PlateAppearances > 0 {
+				return fmt.Sprintf("-%.3f", float64(stick.NegativePoints())/float64(stick.PlateAppearances))
+			}
+			return "0"
+		}(),
+		columnKeyPoints: func() string {
+			return fmt.Sprintf("%d", stick.PositivePoints()-stick.NegativePoints())
+		}(),
+		columnKeyPointsPerPA: func() string {
+			if stick.PlateAppearances > 0 {
+				return fmt.Sprintf("%.3f", float64(stick.PositivePoints()-stick.NegativePoints())/float64(stick.PlateAppearances))
+			}
+			return "0"
+		}(),
+	})
+
+}
+
+func NewModel(sticks []Stick) Model {
+	tableRows := make([]table.Row, len(sticks))
+	for i, stick := range sticks {
+		tableRows[i] = makeRow(stick)
+	}
+
+	return Model{
+		statTable: table.New([]table.Column{
+			table.NewColumn(columnKeyName, "Name", 16).WithStyle(lipgloss.NewStyle().Align(lipgloss.Left)),
+			table.NewColumn(columnKeyPlateAppearances, "PA", 6),
+			table.NewColumn(columnKeyPosPoints, "+P", 6),
+			table.NewColumn(columnKeyPosPointsPerPA, "+P/PA", 6),
+			table.NewColumn(columnKeyNegPoints, "-P", 6),
+			table.NewColumn(columnKeyNegPointsPerPA, "-P/PA", 6),
+			table.NewColumn(columnKeyPoints, "P", 6),
+			table.NewColumn(columnKeyPointsPerPA, "P/PA", 6).WithStyle(lipgloss.NewStyle().Bold(true)),
+		}).WithRows(tableRows).BorderRounded().WithBaseStyle(styleBase).WithPageSize(12).SortByDesc(columnKeyPointsPerPA).Focused(true)}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	m.statTable, cmd = m.statTable.Update(msg)
+	cmds = append(cmds, cmd)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		}
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m Model) View() string {
+	// selected := m.statTable.HighlightedRow().Data[0].(string)
+	view := lipgloss.JoinVertical(
+		lipgloss.Left,
+		styleSubtle.Render("Press ctrl+c to quit."),
+		m.statTable.View(),
+	) + "\n"
+
+	return lipgloss.NewStyle().MarginLeft(1).Render(view)
+}
+
 func main() {
+
 	teamKey := "DET"
 	doc, err := fetchHTML(teamKey)
 	if err != nil {
@@ -127,5 +317,12 @@ func main() {
 		return
 	}
 
-	fmt.Printf("%s data successfully written to %s\n", teamKey, csvFile)
+	// fmt.Printf("%s data successfully written to %s\n", teamKey, csvFile)
+
+	sticks := parseSticks(tableRows)
+
+	p := tea.NewProgram(NewModel(sticks))
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
